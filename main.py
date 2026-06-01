04708942c4cc0b3383dde6fd9687a91540c58fd2
Gosukula Shirisha <gosukulashirisha02@gmail.com>
2026-06-01 16:29:19 +0530

    backend

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api=FastAPI()
+
+client=Groq(
+    api_key=os.getenv("s_key")
+)
+
+@app.post("/generate")
+def generte_content(
+    topic:str,
+    technolofy:str,
+    content_type:str,
+    tone:str
+):
+    prompt=f"""
+    Generate a {content_type}
+    Topic:{topic}
+    Technology:{technology}
+    Tone:{tone}
+    
+    """
+    
+    response=client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        message=[
+            {
+                "role":"user",
+                "content":"prompt"
+            }
+        ]
+    )
+    
+    return{
+        "content":response.choices[0].message.content
+    }
