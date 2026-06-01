39f0757879be62ee0a056d391587136e6f04a4e8
Gosukula Shirisha <gosukulashirisha02@gmail.com>
2026-06-01 18:15:05 +0530

    added openai

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from groq import Groq
+from openai import OpenAI
 
 import os
 
@@ -10,7 +11,7 @@ app=FastAPI()
 def home(): 
     return{ "msg":"content generated successfully" }
 
-client = Groq(
+client = OpenAI(
     api_key=os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1"
 )
